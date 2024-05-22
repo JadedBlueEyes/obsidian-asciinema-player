6380dfcf7f20272a6707cee74ceb6d924ad9452f
denis <dkleinbe@free.fr>
2024-05-22 12:40:57 +0200

    First run

---
+++ src/locales/lang/en.ts
@@ -0,0 +1,10 @@
+export default {
+    FileNotFound: 'file not found',
+    EncounteredAnUnkownError: 'encountered an unknown error',
+    PluginSettings: 'Plugin Settings',
+    EnableOfflineSupport: 'Enable offline support',
+    OfflineSupportOptionDesp: 'Allow plugin run in offline mode',
+    InitializationFailedNotice: 'asciinema-player initialization failed. Plugin will fallback to use online assets. You could turn on %s option to retry this step',
+    DeletionFailedNotice: 'asciinema-player cleanup file failed. This might either happening when file permission changed or files already removed. You could turn off %s option to retry this step',
+    AssetsDownloadFailedNotice: 'asciinema-player assets failed to download'
+}
